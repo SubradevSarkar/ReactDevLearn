c5882689bdd289d8d328bfe8d56b517dd79cd5e2
subradev sarkar <76807055+SubradevSarkar@users.noreply.github.com>
2023-10-27 17:54:48 +0530

    add - currency converter app

--- 6.currencyconvertor/src/App.jsx
+++ 6.currencyconvertor/src/App.jsx
@@ -1,14 +1,66 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
-// import { InputBox } from "./components";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  };
+
+  const handelCurrencyConvert = () => {
+    console.log(amount, from, " -> ", to, currencyInfo[to]);
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
     <>
-      <h1>Currency converter</h1>
-      {/* <InputBox /> */}
+      <div className="flex flex-col items-center mt-10">
+        <h1>Currency converter</h1>
+        <div className="flex flex-col items-center bg-slate-300 bg-opacity-80 rounded-lg p-6 mt-6">
+          <div>
+            <InputBox
+              label="From"
+              amount={amount}
+              currencyOptions={options}
+              onAmountChange={(amount) => setAmount(amount)}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              selectCurrency={from}
+            />
+          </div>
+          <button
+            className="px-3 py-1 -mt-2 -mb-2 z-50 shadow-lg bg-blue-600 rounded-md "
+            onClick={swap}
+          >
+            swap
+          </button>
+          <InputBox
+            label="To"
+            amount={convertedAmount}
+            currencyOptions={options}
+            onCurrencyChange={(currency) => setTo(currency)}
+            selectCurrency={to}
+            amountDisable
+          />
+          <button
+            className="px-3 py-1 mt-4 z-50 shadow-lg bg-blue-600 rounded-md "
+            onClick={handelCurrencyConvert}
+          >
+            Convert {from.toUpperCase()} to {to.toUpperCase()}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- 6.currencyconvertor/src/components/InputBox.jsx
+++ 6.currencyconvertor/src/components/InputBox.jsx
@@ -1,34 +1,46 @@
+/* eslint-disable react/prop-types */
 import { useId } from "react";
+import PropTypes from "prop-types";
 
 const InputBox = ({
   label,
   amount,
   onAmountChange,
-  onCurrentChange,
+  onCurrencyChange,
   currencyOptions = [],
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
-  className = "",
 }) => {
   const amountFormId = useId();
   return (
-    <div className="flex justify-center align-middle px-1 py-2">
-      <div className="flex flex-col justify-start">
-        <label htmlFor={amountFormId}>{label}</label>
+    <div className="flex justify-center items-center gap-6 p-8 shadow-md bg-slate-100 rounded-lg">
+      <div className="flex flex-col gap-3 text-slate-800 justify-start">
+        <label htmlFor={amountFormId} className="font-medium">
+          {label}
+        </label>
         <input
           id={amountFormId}
           type="number"
+          placeholder="Amount"
           value={amount}
           onChange={(e) =>
             onAmountChange && onAmountChange(Number(e.target.value))
           }
           disabled={amountDisable}
+          className="py-1  bg-slate-300 text-black ps-3 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
         />
       </div>
-      <div className="flex flex-col justify-end">
-        <label htmlFor="">Currency Type</label>
-        <select name="" id="">
+      <div className="flex flex-col gap-3 text-slate-800 justify-end">
+        <label className="font-medium">Currency Type</label>
+        <select
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          value={selectCurrency}
+          disabled={currencyDisable}
+          className="py-1.5 bg-gray-900 text-white ps-3 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        >
           {currencyOptions &&
             currencyOptions.map((currency) => (
               <option key={currency} value={currency}>
@@ -39,6 +51,16 @@ const InputBox = ({
       </div>
     </div>
   );
+};
+InputBox.prototype = {
+  label: PropTypes.string,
+  amount: PropTypes.number,
+  onAmountChange: PropTypes.func,
+  onCurrencyChange: PropTypes.func,
+  currencyOptions: PropTypes.array,
+  selectCurrency: PropTypes.string,
+  amountDisable: PropTypes.bool,
+  currencyDisable: PropTypes.bool,
 };
 
 export default InputBox;
