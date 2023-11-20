d2ef221492a65f0e111035fd0b261d4127827d64
subradev sarkar <76807055+SubradevSarkar@users.noreply.github.com>
2023-11-21 02:19:45 +0530

    add config appwrite

--- Inscribe/src/App.jsx
+++ Inscribe/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import conf from "./conf/conf.js";
 
 import "./App.css";
 
 function App() {
+  console.log(505, conf.appwriteUrl);
   const [count, setCount] = useState(0);
 
   return <>count{count}</>;
---
+++ Inscribe/src/conf/conf.js
@@ -0,0 +1,11 @@
+const ENV = import.meta.env;
+
+const conf = {
+  appwriteUrl: String(ENV.VITE_APPWRITE_URL),
+  appwriteProjectId: String(ENV.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(ENV.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(ENV.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(ENV.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
