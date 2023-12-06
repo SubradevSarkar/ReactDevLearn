60539d901efb1eae925e5d4c7e45a76f02cb9b9e
subradev sarkar <76807055+SubradevSarkar@users.noreply.github.com>
2023-12-06 08:19:07 +0530

    update

--- Inscribe/src/components/index.js
+++ Inscribe/src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Header } from "./Header/Header.jsx";
 export { default as Footer } from "./Footer/Footer.jsx";
+export { default as Container } from "./container/Container.jsx";
+export { default as Logout } from "./Header/Logout.jsx";
+export { default as Button } from "./Button/Button.jsx";
+export { default as Logo } from "./Logo/Logo.jsx";
