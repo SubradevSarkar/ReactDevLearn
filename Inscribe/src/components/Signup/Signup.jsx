c667c2bdda9caa1f778e569967a2e62664d2f485
subradev sarkar <subradevssarkar@gmail.com>
2023-12-07 14:17:07 +0530

    update

--- Inscribe/src/components/Signup/Signup.jsx
+++ Inscribe/src/components/Signup/Signup.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { useState } from "react";
+import { Button, Input, Logo } from "../index";
+import { useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import authService from "../../appwrite/auth";
+import { login } from "../../store/authSlice";
+import { useForm } from "react-hook-form";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const user = await authService.getCurrentUser();
+        if (user) dispatch(login(user));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border-black/10">
+        <div className="mb-2 flex justify-center">
+          <span className=" inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your account
+        </h2>
+        <p className=" mt-2 text-center text-base text-black/60">
+          Already have any account?&nbsp;
+          <Link
+            to="/login"
+            className=" font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign In
+          </Link>
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
+          <div className=" space-y-5">
+            <Input
+              label="Name:"
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Password:"
+              placeholder="Enter your full Password"
+              {...register("password", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Email:"
+              placeholder="Enter your email"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPattern: (pattern) =>
+                    /^w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                      pattern
+                    ) || "Email address must be a valid address",
+                },
+              })}
+            />
+            <Button type="submit" className="w-full">
+              Create account
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
--- Inscribe/src/components/index.js
+++ Inscribe/src/components/index.js
@@ -6,6 +6,7 @@ export { default as Logout } from "./Header/Logout.jsx";
 export { default as Login } from "./Login/Login.jsx";
 export { default as Signup } from "./Signup/Signup.jsx";
 export { default as PostCard } from "./PostCard/PostCard.jsx";
+export { default as AuthLayout } from "./AuthLayout/AuthLayout.jsx";
 
 // elements export
 export { default as Button } from "./elements/Button/Button.jsx";
